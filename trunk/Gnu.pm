5135f9cb7ed0cfd269b51190057095ca68be23d0
hayashi <hayashi@75bfd316-ca9c-4271-b570-2f09a026c862>
2017-11-19 14:27:20 +0000

    delete a "#" for debug.

--- trunk/Gnu.pm
+++ trunk/Gnu.pm
@@ -83,7 +83,7 @@ END
 # use Term::ReadLine::Stub on a dumb terminal.
 # https://rt.cpan.org/Ticket/Display.html?id=123398
 BEGIN {
-#    croak "dumb terminal." if($ENV{TERM} =~ /^(dumb|emacs|unknown)$/);
+    croak "dumb terminal." if($ENV{TERM} =~ /^(dumb|emacs|unknown)$/);
 }
 
 {
